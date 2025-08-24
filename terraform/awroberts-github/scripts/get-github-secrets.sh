90b476cdd4e0b6897b165cea54a8f9eb4d62f8b1
awroberts <awr@awroberts.co.uk>
2025-08-24 23:43:36 +0100

    Updated github repository declaration

--- terraform/awroberts-github/scripts/get-github-secrets.sh
+++ terraform/awroberts-github/scripts/get-github-secrets.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 
-GITHUB_REPOSITORY_WITH_REMOVED_ORGANIZATION_PREFIX=$(echo "${AWROBERTS_GITHUB_REPOSITORY}" | cut -d'/' -f2)
-
 cat <<EOF > github-terraform.auto.tfvars
-awroberts_github_repository = "${GITHUB_REPOSITORY_WITH_REMOVED_ORGANIZATION_PREFIX}"
+awroberts_github_repository = "${AWROBERTS_GITHUB_REPOSITORY}"
 awroberts_github_token = "${AWROBERTS_GITHUB_TOKEN}"
 awroberts_tf_cloud_terraform_version = "${TF_CLOUD_TERRAFORM_VERSION}"
 awroberts_tfe_token = "${TFE_TOKEN}"
