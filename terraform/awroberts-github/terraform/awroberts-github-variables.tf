e39c346aa9dda2da22a357f756674d37ea1c69d5
awroberts <awr@awroberts.co.uk>
2025-08-24 23:09:01 +0100

    Github Variables

---
+++ terraform/awroberts-github/terraform/awroberts-github-variables.tf
@@ -0,0 +1,19 @@
+variable "awroberts_github_token" {
+  type = string
+}
+
+variable "awroberts_tf_cloud_terraform_version" {
+  type = string
+}
+
+variable "awroberts_tf_cloud_workspace_id" {
+  type = string
+}
+
+variable "awroberts_tfe_token" {
+  type = string
+}
+
+variable "awroberts_github_repository" {
+  type = string
+}
