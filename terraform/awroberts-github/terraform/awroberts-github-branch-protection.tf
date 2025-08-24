e84d531fe4032a085c1056c42a36d4a5eb352975
awroberts <awr@awroberts.co.uk>
2025-08-24 23:08:22 +0100

    Github Branch Protection

---
+++ terraform/awroberts-github/terraform/awroberts-github-branch-protection.tf
@@ -0,0 +1,14 @@
+data "github_repository" "awroberts" {
+  name = var.awroberts_github_repository
+}
+
+resource "github_branch_protection" "awroberts_main" {
+  repository_id = data.github_repository.awroberts.node_id
+  pattern       = "main"
+
+  required_status_checks {
+    strict = true
+  }
+
+  enforce_admins = true
+}
