8c78c2f416dbca94ab69da99d09620580774aefd
awroberts <awr@awroberts.co.uk>
2025-09-14 21:10:35 +0100

    Updated to comment out deprecated sysctl warmings

--- scripts/functions/common/setup-kubernetes-networking.sh
+++ scripts/functions/common/setup-kubernetes-networking.sh
@@ -17,17 +17,13 @@ net.bridge.bridge-nf-call-ip6tables = 1
 net.ipv4.ip_forward = 1
 EOF
 
-  # Suppress deprecated sysctl warnings
-  sudo tee /etc/sysctl.d/99-disable-invalid.conf >/dev/null <<EOF
-# Suppress invalid sysctl keys from default config
-net.ipv4.conf.all.accept_source_route =
-net.ipv4.conf.all.promote_secondaries =
-EOF
+  # Comment out invalid sysctl keys in default config (if present)
+  sudo sed -i 's/^\(net\.ipv4\.conf\.all\.accept_source_route\)/# \1/' /usr/lib/sysctl.d/50-default.conf
+  sudo sed -i 's/^\(net\.ipv4\.conf\.all\.promote_secondaries\)/# \1/' /usr/lib/sysctl.d/50-default.conf
 
   # Reload sysctl settings
   sudo sysctl --system >/dev/null
 
   export SYSCTL_ALREADY_APPLIED=true
-
   echo "✅ Kubernetes networking sysctls applied successfully."
 }
