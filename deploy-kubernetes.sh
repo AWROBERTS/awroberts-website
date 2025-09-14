b7cdf9b266d739cd68cdc105d21f17090996a53e
awroberts <awr@awroberts.co.uk>
2025-09-14 20:04:19 +0100

    Updated logic to load env vars first

--- deploy-kubernetes.sh
+++ deploy-kubernetes.sh
@@ -1,19 +1,26 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# Try the most robust way to always get project root (will work even if sh is the shell)
+# Get project root
 SCRIPT_PATH="$(readlink -f "$0" 2>/dev/null || realpath "$0" 2>/dev/null || echo "$0")"
 PROJECT_ROOT="$(cd "$(dirname "$SCRIPT_PATH")" && pwd)"
-
 echo "PROJECT_ROOT: $PROJECT_ROOT"
 
+# Load env loader first
+for file in "${PROJECT_ROOT}/scripts/functions/env/"*.sh; do
+  [[ "$file" == *load_env_file.sh ]] && continue  # Already sourced
+  source "$file"
+done
+load_env_file  # Load .env variables before anything else
+
+# Now source all other functions
 for file in "${PROJECT_ROOT}/scripts/functions/common/"*.sh; do
+  [[ "$file" == *load_env_file.sh ]] && continue  # Already sourced
   source "$file"
 done
 
 main() {
   sudo_if_needed
-  load_env_file
   setup_kubernetes_networking
   image_vars
   ensure_tls_secret
