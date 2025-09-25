1270de47c9c606b110879c9040b5f65315eff541
awroberts <awr@awroberts.co.uk>
2025-09-25 11:03:25 +0100

    Updated to reference bgvideo functionality

--- docker/awroberts/sketch.js
+++ docker/awroberts/sketch.js
@@ -1,3 +1,5 @@
+let bgVideo;
+
 function setup() {
   const canvas = createCanvas(windowWidth, windowHeight);
   canvas.parent('canvas-container');
@@ -5,8 +7,20 @@ function setup() {
   canvas.style('top', '0');
   canvas.style('left', '0');
   canvas.style('z-index', '1');
+
+  bgVideo = createVideo('background.mp4');
+  bgVideo.parent('canvas-container');
+  bgVideo.size(windowWidth, windowHeight);
+  bgVideo.style('z-index', '0');
+  bgVideo.style('object-fit', 'cover');
+  bgVideo.style('position', 'absolute');
+  bgVideo.style('top', '0');
+  bgVideo.style('left', '0');
+  bgVideo.loop();
+  bgVideo.hide();
 }
 
 function draw() {
-  background(255, 255, 255);
+  fill(255, 255, 255, 50);
+  rect(0, 0, width, height);
 }
