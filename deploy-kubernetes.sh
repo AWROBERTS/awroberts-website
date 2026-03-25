1aa7d684d534435da95f62452e55201d98b19928
awroberts <awr@awroberts.co.uk>
2026-03-25 22:02:10 +0000

    Updated to reference ensure metallb crds

--- deploy-kubernetes.sh
+++ deploy-kubernetes.sh
@@ -67,7 +67,7 @@ main() {
   build_image
   import_image
   restart_kube_proxy
-  ensure_metallb_crd
+  ensure_metallb_crds
   ensure_metallb_helm
   ensure_metallb_config_helm
   ensure_traefik_helm
