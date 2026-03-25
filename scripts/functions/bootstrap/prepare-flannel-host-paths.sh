8d0adfdc33868bcf49eaaa289039bf364c910466
awroberts <awr@awroberts.co.uk>
2026-03-25 09:21:09 +0000

    Updated to include chmod

--- scripts/functions/bootstrap/prepare-flannel-host-paths.sh
+++ scripts/functions/bootstrap/prepare-flannel-host-paths.sh
@@ -1,5 +1,11 @@
 prepare_flannel_host_paths() {
   echo "Creating required host paths for Flannel..."
-  sudo_if_needed mkdir -p /opt/cni/bin /etc/cni/net.d /run/flannel
-  sudo_if_needed chmod 755 /etc/cni/net.d /run/flannel
+  sudo_if_needed mkdir -p /opt/cni/bin
+  sudo_if_needed mkdir -p /etc/cni/net.d
+  sudo_if_needed mkdir -p /run/flannel
+
+  sudo_if_needed chmod 755 /opt/cni/bin
+  sudo_if_needed chmod 755 /etc/cni/net.d
+  sudo_if_needed chmod 755 /run/flannel
 }
+
