0eb7d9c6e15dcaacd187c6ac247d9f94ee326644
awroberts <awr@awroberts.co.uk>
2026-03-26 18:13:52 +0000

    Removed unneeded file

--- scripts/functions/image/ensure-legacy-iptables.sh
+++
@@ -1,8 +0,0 @@
-ensure_legacy_iptables() {
-  echo "🛠 Ensuring legacy iptables backend is active..."
-
-  sudo update-alternatives --set iptables /usr/sbin/iptables-legacy
-  sudo update-alternatives --set ip6tables /usr/sbin/ip6tables-legacy
-
-  echo "✔ iptables backend set to legacy"
-}
