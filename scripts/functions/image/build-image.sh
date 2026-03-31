553de2ade0609b3b5419b006c156f7da695c33ac
awroberts <awr@awroberts.co.uk>
2026-03-31 21:15:53 +0100

    Removed unneeded file

--- scripts/functions/image/build-image.sh
+++
@@ -1,8 +0,0 @@
-build_image() {
-  echo "🔨 Building image ${FULL_IMAGE} and tagging as latest for ${PLATFORM}"
-  docker build \
-    --no-cache \
-    -t "${FULL_IMAGE}" \
-    -t "${LATEST_IMAGE}" \
-    "${BUILD_CONTEXT}"
-}
