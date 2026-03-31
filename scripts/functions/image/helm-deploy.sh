7272598bb75f739e7d1c29909ae7ecde0ad8c824
awroberts <awr@awroberts.co.uk>
2026-03-31 22:46:02 +0100

    Updated to reference background image

--- scripts/functions/image/helm-deploy.sh
+++ scripts/functions/image/helm-deploy.sh
@@ -5,14 +5,18 @@ deploy_with_helm() {
   kubectl create namespace "${NAMESPACE}" --dry-run=client -o yaml | kubectl apply -f -
 
   helm upgrade --install "${DEPLOYMENT_NAME}" "${HELM_CHART_PATH}" \
-    --namespace "${NAMESPACE}" \
-    --set image.repository="${IMAGE_NAME_BASE}" \
-    --set image.tag="${IMAGE_TAG}" \
-    --set image.pullPolicy="Never" \
-    --set traefik.tls.secretName="${SECRET_NAME}" \
-    --set traefik.hostnames[0]="${HOST_A}" \
-    --set traefik.hostnames[1]="${HOST_B}" \
-    --set volume.hostPath="${HOST_MEDIA_PATH}" \
-    --set volume.mountPath="/usr/share/nginx/html/awroberts-media"
+  --namespace "${NAMESPACE}" \
+  --set image.repository="${APP_IMAGE_NAME_BASE}" \
+  --set image.tag="${IMAGE_TAG}" \
+  --set image.pullPolicy="Never" \
+  --set backgroundVideo.image.repository="${BACKGROUND_IMAGE_NAME_BASE}" \
+  --set backgroundVideo.image.tag="${IMAGE_TAG}" \
+  --set backgroundVideo.image.pullPolicy="Never" \
+  --set traefik.tls.secretName="${SECRET_NAME}" \
+  --set traefik.hostnames[0]="${HOST_A}" \
+  --set traefik.hostnames[1]="${HOST_B}" \
+  --set volume.hostPath="${HOST_MEDIA_PATH}" \
+  --set volume.mountPath="/usr/share/nginx/html/awroberts-media"
+
 }
 
