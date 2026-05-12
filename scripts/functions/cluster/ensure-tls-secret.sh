0753c10b87cbff801a15430f62778087ebced3a5
awroberts <awr@awroberts.co.uk>
2026-05-12 20:28:20 +0100

    Updated to reference awroberts namespace

--- scripts/functions/cluster/ensure-tls-secret.sh
+++ scripts/functions/cluster/ensure-tls-secret.sh
@@ -1,8 +1,8 @@
 ensure_tls_secret() {
-  kubectl get ns traefik >/dev/null 2>&1 || kubectl create namespace traefik
+  kubectl get ns "${NAMESPACE}" >/dev/null 2>&1 || kubectl create namespace "${NAMESPACE}"
 
-  kubectl -n traefik create secret tls "$SECRET_NAME" \
-    --cert="$HOST_CERT_PATH" \
-    --key="$HOST_KEY_PATH" \
+  kubectl -n "${NAMESPACE}" create secret tls "${SECRET_NAME}" \
+    --cert="${HOST_CERT_PATH}" \
+    --key="${HOST_KEY_PATH}" \
     --dry-run=client -o yaml | kubectl apply -f -
 }
