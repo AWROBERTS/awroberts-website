89964e8cc96bd019abb2136a745ff7d5f768194c
awroberts <awr@awroberts.co.uk>
2026-05-17 14:25:53 +0100

    Updated to reference location flag

--- scripts/functions/cluster/ensure-k8s-and-containerd-installed.sh
+++ scripts/functions/cluster/ensure-k8s-and-containerd-installed.sh
@@ -4,7 +4,7 @@ ensure_k8s_and_containerd_installed() {
   sudo_if_needed apt-get update
   sudo_if_needed apt-get install -y apt-transport-https ca-certificates curl gpg
 
-  curl -fsSL https://pkgs.k8s.io/core:/stable:/v1/deb/Release.key \
+  curl -fsSL --location https://pkgs.k8s.io/core:/stable:/v1/deb/Release.key \
     | sudo_if_needed tee /usr/share/keyrings/kubernetes-archive-keyring.gpg >/dev/null
 
   echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] \
