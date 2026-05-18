d70cdd43164487986affcc8e4536aaaee618806f
awroberts <awr@awroberts.co.uk>
2026-05-18 10:32:44 +0100

    Updated to reference kubeadm version

--- scripts/functions/bootstrap/initialise-kubeadm.sh
+++ scripts/functions/bootstrap/initialise-kubeadm.sh
@@ -1,4 +1,6 @@
 initialise_kubeadm() {
-  echo "Initialising control plane with pod CIDR ${POD_CIDR}..."
-  sudo_if_needed kubeadm init --pod-network-cidr="${POD_CIDR}"
+  echo "Initialising control plane with pod CIDR ${POD_CIDR} and Kubernetes ${VERSION}..."
+  sudo_if_needed kubeadm init \
+    --kubernetes-version="${VERSION}" \
+    --pod-network-cidr="${POD_CIDR}"
 }
