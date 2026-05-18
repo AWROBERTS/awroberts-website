143e50e177ff592de4547ce07fdd69a443ceea02
awroberts <awr@awroberts.co.uk>
2026-05-18 11:29:50 +0100

    Removed duplicate sysctl configuration

--- scripts/functions/bootstrap/disable-swap-and-configure-sysctls.sh
+++ scripts/functions/bootstrap/disable-swap-and-configure-sysctls.sh
@@ -6,12 +6,4 @@ disable_swap_and_configure_sysctls() {
   echo "Ensuring br_netfilter kernel module is loaded"
   sudo_if_needed modprobe br_netfilter || true
   echo 'br_netfilter' | sudo_if_needed tee /etc/modules-load.d/br_netfilter.conf >/dev/null
-
-  echo "Configuring kernel sysctls for bridged traffic and forwarding..."
-  sudo_if_needed tee /etc/sysctl.d/99-kubernetes-cri.conf >/dev/null <<EOF
-net.bridge.bridge-nf-call-iptables = 1
-net.bridge.bridge-nf-call-ip6tables = 1
-net.ipv4.ip_forward = 1
-EOF
-  sudo_if_needed sysctl --system >/dev/null
 }
