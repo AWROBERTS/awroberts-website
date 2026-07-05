890b81a2c62df5927fdcc4dfb5f99f1d756e3e33
awroberts <awr@awroberts.co.uk>
2026-07-05 10:49:50 +0100

    Updated to delete old Flannel CNI installations

--- scripts/functions/bootstrap/install-flannel-cni.sh
+++ scripts/functions/bootstrap/install-flannel-cni.sh
@@ -1,4 +1,7 @@
 install_flannel_cni() {
+  echo "Removing Flannel CNI..."
+  kubectl delete daemonset kube-flannel-ds -n kube-flannel --ignore-not-found
+  kubectl delete -f https://github.com/flannel-io/flannel/releases/latest/download/kube-flannel.yml --ignore-not-found
   echo "Installing Flannel CNI..."
   kubectl apply -f https://github.com/flannel-io/flannel/releases/latest/download/kube-flannel.yml
 }
