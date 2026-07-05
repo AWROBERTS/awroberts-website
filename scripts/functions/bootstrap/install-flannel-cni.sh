d9b2ed5e65d8a77bf107f77bef0502e8afd240fa
awroberts <awr@awroberts.co.uk>
2026-07-05 10:45:12 +0100

    Updated flannel manifest URL

--- scripts/functions/bootstrap/install-flannel-cni.sh
+++ scripts/functions/bootstrap/install-flannel-cni.sh
@@ -1,4 +1,4 @@
 install_flannel_cni() {
   echo "Installing Flannel CNI..."
-  kubectl apply -f https://raw.githubusercontent.com/flannel-io/flannel/main/Documentation/kube-flannel.yml
+  kubectl apply -f https://github.com/flannel-io/flannel/releases/latest/download/kube-flannel.yml
 }
