9a583f41a3a9dedda1d1db35a89ee0f835b8c893
awroberts <awr@awroberts.co.uk>
2026-07-06 21:33:17 +0100

    Flannel multi-arch CNI installation

--- scripts/functions/bootstrap/install-flannel-cni.sh
+++ scripts/functions/bootstrap/install-flannel-cni.sh
@@ -1,7 +1,15 @@
 install_flannel_cni() {
-  echo "Removing Flannel CNI..."
-  kubectl delete daemonset kube-flannel-ds -n kube-flannel --ignore-not-found
-  kubectl delete -f https://github.com/flannel-io/flannel/releases/latest/download/kube-flannel.yml --ignore-not-found
-  echo "Installing Flannel CNI..."
-  kubectl apply -f https://github.com/flannel-io/flannel/releases/latest/download/kube-flannel.yml
+  echo "Removing any existing Flannel CNI..."
+
+  # Delete Flannel from the correct namespace
+  kubectl delete daemonset kube-flannel-ds -n kube-system --ignore-not-found
+
+  # Delete any old Flannel manifests that may have been applied
+  kubectl delete -f https://raw.githubusercontent.com/flannel-io/flannel/master/Documentation/kube-flannel.yml --ignore-not-found
+
+  echo "Installing multi-arch Flannel CNI..."
+  kubectl apply -f https://raw.githubusercontent.com/flannel-io/flannel/master/Documentation/kube-flannel.yml
+
+  echo "Waiting for Flannel pods to become Ready..."
+  kubectl rollout status daemonset/kube-flannel-ds -n kube-system
 }
