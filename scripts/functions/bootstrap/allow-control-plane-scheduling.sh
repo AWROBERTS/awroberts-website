763e8b77e87898527d3a6e657e30f22be86fff1b
awroberts <awr@awroberts.co.uk>
2026-07-08 21:27:30 +0100

    Removed unneeded file

--- scripts/functions/bootstrap/allow-control-plane-scheduling.sh
+++
@@ -1,8 +0,0 @@
-allow_control_plane_scheduling() {
-  echo "⚙️ Allowing workloads to schedule on the control plane node"
-
-  kubectl taint nodes "$CONTROL_PLANE_HOST" node-role.kubernetes.io/control-plane:NoSchedule-
-  kubectl taint nodes "$CONTROL_PLANE_HOST" node-role.kubernetes.io/master:NoSchedule- || true
-
-  echo "✔️ Control plane node is now schedulable"
-}
