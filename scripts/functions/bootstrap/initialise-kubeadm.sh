af99eba26c74e24ec58bf6c2816d7eda6ac99578
awroberts <awr@awroberts.co.uk>
2026-07-08 09:18:01 +0100

    Removed unneeded file

--- scripts/functions/bootstrap/initialise-kubeadm.sh
+++
@@ -1,6 +0,0 @@
-initialise_kubeadm() {
-  echo "Initialising control plane with pod CIDR ${POD_CIDR} and Kubernetes ${VERSION}..."
-  sudo_if_needed kubeadm init \
-    --kubernetes-version="${VERSION}" \
-    --pod-network-cidr="${POD_CIDR}"
-}
